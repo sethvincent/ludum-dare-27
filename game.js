6e52028c3526b326ff9962ad99f878fd350d8613
sethvincent <sethvincent@gmail.com>
2013-08-23 18:54:00 -0700

    player moves

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 var Game = require('crtrdg-gameloop');
 var Keyboard = require('crtrdg-keyboard');
 var Mouse = require('crtrdg-mouse');
+var Player = require('./player')
 
 var game = new Game({
   canvasId: 'game',
@@ -35,3 +36,36 @@ game.on('draw', function(context){});
 game.on('pause', function(){});
 
 game.on('resume', function(){});
+
+
+/* create player */
+var player = new Player({
+  size: {
+    x: 8,
+    y: 12
+  },
+  position: {
+    x: game.width / 2 - 5,
+    y: game.height / 2 - 5,
+  },
+  color: '#cfcfc2',
+  speed: 5
+});
+
+player.visible = true;
+player.addTo(game);
+
+player.on('update', function(interval){
+  this.input(keyboard);
+  this.move();
+  this.velocity.x = 0;
+  this.velocity.y = 0;
+  this.boundaries();
+});
+
+player.on('draw', function(context){
+  if (player.visible){
+    context.fillStyle = this.color;
+    context.fillRect(this.position.x, this.position.y, this.size.x, this.size.y);    
+  }
+});
